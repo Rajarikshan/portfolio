c48c331576f7384e797fd47abd982d38cf60a035
rajarikshan <rajarikshan2143@gmail.com>
2025-02-27 08:30:23 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,109 @@
+// function myMenuFunction(){
+//     var menuBth=document.getElementById("myNavmenu");
+//     if(menuBth.className==="nav-menu"){
+//         menuBth.className += "responsive"
+//     }else{
+//         menuBth.className="nav-menu";
+//     }
+// }
+
+
+
+// dark mode
+
+
+
+const body =document.querySelector("body"),
+toggleswitch=document.getElementById("toggleswitch");
+
+toggleswitch.addEventListener("click",()=>{
+    body.classList.toggle("dark");
+});
+
+
+// typing effect
+
+// var typingEffect=new typed(".typeText",{
+//     Strings:["Designer","coder","developers"],
+
+//     loop:true,
+//     typeSpeed:100,
+//     backSpeed:80,
+//     backDelay:2000,
+
+// })
+
+
+
+// --------scroll animation------------
+
+// const sr=scrollReveal({
+//     origin:"top",
+//     distance:"80px",
+//     duration:2000,
+//     reset:true,
+// });
+
+// sr.reveal(".featured-name",{delay:100});
+// sr.reveal(".text-info",{delay:200});
+// sr.reveal(".text-btn",{delay:200});
+// sr.reveal(".social_icon",{delay:200});
+// sr.reveal(".featured-image",{delay:200});
+
+
+// sr.reveal(".project-box",{Interval:200});
+
+
+// sr.reveal(".top-header",{})
+
+// const srLeft=scrollReveal({
+//     origin:"left",
+//     distance:"80px",
+//     duration:2000,
+//     reset:true
+// })
+
+
+
+
+
+// srLeft.reveal(".about-info",{delay:100});
+// srLeft.reveal(".cantact-info",{delay:100});
+
+
+
+// const srRight=scrollReveal({
+
+//     origin:"left",
+//     distance:"80px",
+//     duration:2000,
+//     reset:true
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
